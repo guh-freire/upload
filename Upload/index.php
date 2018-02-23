0800088605be15ac654d6a17a3fb6a0d5e095843
guh-freire <35299192+guh-freire@users.noreply.github.com>
2018-02-22 22:40:11 -0300

    Update index.php

--- Upload/index.php
+++ Upload/index.php
@@ -8,7 +8,7 @@ if($_POST['submit'] == "Upload Todos"){
 	include('config/MultiploUpLoadTree.php');
 	$multiploUpload = new MultiploUpload($_FILES['arquivo']['name'] , $_filial['filial'] , $_nome['nome'] , $_FILES['arquivo']['size'] , $_FILES['arquivo']['tmp_name'] , $_FILES['arquivo']['type']);
 	
-header("refresh: 1; url=http://www.fazenda.org.br"); 
+header(""); 
 }
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -26,48 +26,12 @@ header("refresh: 1; url=http://www.fazenda.org.br");
 <body>
 	<div>
 	<fieldset>
-        	<legend>Enviar Documentos</legend>
+        	<legend>TITULO/legend>
         	<small><font face="verdana" color="#000000" > 
-			<b>Trabalhador:<br></b>
-			● CPF – Cadastro de Pessoa Física<br>
-			● Cartão PIS-PASEP - Inscrição INSS<br>
-			● Comprovante de Escolaridade<br>
-			● Certidão de Nascimento ou Certidão de Casamento<br>
-			● CTPS: Número de Série, Qualificação Civil, Alterações de Identidade<br>
-			● DNI (Caso Possua)<br>
-			● RG – Registro Geral<br>
-			● Registro Nacionalidade Estrangeira (Caso seja Estrangeiro)<br>
-			● Inscrição Órgão de Classe (Encontra na Carteira de Trabalho, caso possua ensino superior)<br>
-			● CNH – Carteira Nacional de Habilitação (Caso Possua)<br>
-			● Comprovante de Residência<br>
-			● Trabalhador Estrangeiro – Todos Documentos citados<br><br>
-			
-			<b>Dependentes:</b><br>
-			● RG, Certidão de Nascimento e CPF dos dependentes<br>
-			● Preencher ficha de Dependentes, assinar e enviar digitalizada junto dos demais documentos: <a href="">Download Ficha</a> <br><br>
-			<font face="verdana" color="red"> <b><small><small>(Clicar em download para baixar a ficha de “Declaração de Encargos de Familia para fins de Imposto de Renda”. Assinar e enviar digitalizada junto dos demais documentos.)<br>
-			Envio de arquivos; digitalizar os documentos com resolucão de 100 x 100 dpi.</b>
+			<b>TITULO<br></b>
 			
     </fieldset><br><br></small></small></font>
-	
-<form action="index.php" method="POST" enctype="multipart/form-data">	
-<font face="verdana" color="#000000" > 
-<fieldset>	
-<legend> Informar Dados </legend>
-     <tr>
-      <td>Filial:</td>
-      <td><input name="filial" type="text" id="filial" size="4" maxlength="4" />
-		<span class="style1"></span></td><br>
-	</tr>  
-	
-	<tr>
-      <td width="69">Nome:</td>
-      <td width="546"><input name="nome" type="text" id="nome" size="30" maxlength="70" />
-        <span class="style1"></span></td><br>
-    </tr>
-    </fieldset>
-</div>	
- 
+
 
 </br>
 
